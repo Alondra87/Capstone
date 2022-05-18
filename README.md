1fa841ba1dfb5dd3217f68d265b384d34e0a4d25
Alondra87 <aa5887687@gmail.com>
2022-05-18 11:10:13 -0400

    edited JS

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
 # Capstone
-This final project is based on an online website for a conference. Instead of a conference I created a website for a cultural event.
+
+> This final project is based on an online website for a conference. Instead of a conference I created a website for a cultural event.
+
+
+## Built With
+
+- HTML and CSS
+- GitHub
+- JavaScript
+- Visual Code Studio
+
+## Live Demo (if available)
+
+[Live Demo Link](https://livedemo.com)
+
+
+## Getting Started
+
+**This is an example of how you may give instructions on setting up your project locally.**
+**Modify this file to match your project, remove sections that don't apply. For example: delete the testing section if the currect project doesn't require testing.**
+
+
+To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+
+### Setup
+
+### Install
+
+### Usage
+
+### Run tests
+
+### Deployment
+
+
+
+## Authors
+
+👤 **Author1**
+
+- GitHub: [@Alondra87](https://github.com/Alondra87)
+- Twitter: @AndreaA07211436
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/andrea-a-384903224/)
+
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used
+- Inspiration
+- etc
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
+
--- script.js
+++ script.js
@@ -11,3 +11,54 @@ document.querySelectorAll('.nav-link').forEach((n) => n
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
   }));
+
+const speakers = [{
+  photo: './img/speaker_01.png',
+  name: 'Carlos Garcia',
+  profession: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+  description: 'Suscipit aliqua and enim corporis dolore nulla. Numquam odit tempor. Tempor sequi. Ea iste eius quis so dolor.',
+}, {
+  photo: './img/speaker_02.png',
+  name: 'David Ramos',
+  profession: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+  description: 'Commodi non, yet magni for elit aspernatur yet deserunt nequeporro. Ipsam aut dolores si fugit for quo. Nisi.',
+}, {
+  photo: './img/speaker_03.png',
+  name: 'Jhon Castillo',
+  profession: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+  description: 'Labore modi sint or id yet numquam. Dolore eiusmod aliqua. Odit commodi so natus nihil and eos quo yet dolore.',
+}, {
+  photo: './img/speaker_04.png',
+  name: 'Gabriela De la Cruz',
+  profession: 'Aute fugit and nisi but fugit and laboris natus',
+  description: 'Elit beatae voluptatem and totam. Irure labore corporis but et pariatur but perspiciatis. Commodi rem doloremque so error quae yet autem for dolores.',
+}, {
+  photo: './img/speaker_05.png',
+  name: 'Milagros Romero',
+  profession: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+  description: 'Nesciunt incidunt, aute so iure. Quia veritatis yet sit dolore beatae but voluptatem minima. Aspernatur. Lorem amet.',
+}, {
+  photo: './img/speaker_06.png',
+  name: 'Maria Zambrana',
+  profession: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+  description: 'Ad magnam for sunt so esse numquam and cupidatat but sit. Lorem amet. Adipisicing ratione and do so veritatis quo adipisci excepteur. Dolorem consequat.',
+}];
+
+const loadSpeakers = () => {
+  let showSpeakers = '';
+
+  for (let i = 0; i < speakers.length; i += 1) {
+    showSpeakers += `<article class="speaker">
+   <img src="${speakers[i].photo}" alt="speaker_01">
+   <div class="curriculum">
+   <h3>${speakers[i].name}</h3>
+   <h4>${speakers[i].profession}</h4>
+   <p>${speakers[i].description}</p>
+   </div>
+  </article>`;
+
+    document.getElementById('speaker').innerHTML = showSpeakers;
+  }
+};
+
+loadSpeakers();
